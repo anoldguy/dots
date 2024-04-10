9d9364d6eeb326fc5aca2f0467733b38c5310787
Nathan Anderson <andnat@gmail.com>
2024-04-10 09:11:20 -0400

    Simplify the fish_ssh_agent; use idiomatic fish

--- .config/fish/functions/fish_ssh_agent.fish
+++ .config/fish/functions/fish_ssh_agent.fish
@@ -1,23 +1,13 @@
 function __ssh_agent_is_started -d "check if ssh agent is already started"
-    if begin
-            test -f $SSH_ENV; and test -z "$SSH_AGENT_PID"
-        end
-        source $SSH_ENV >/dev/null
-    end
-
-    if test -z "$SSH_AGENT_PID"
-        return 1
-    end
-
-    ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep -q ssh-agent
-    return $status
+    set -q SSH_AGENT_PID; or return 1
+    ps -p $SSH_AGENT_PID > /dev/null
 end
 
 
 function __ssh_agent_start -d "start a new ssh agent"
-    ssh-agent -c | grep -v echo >$SSH_ENV
+    ssh-agent -c | grep -v echo > $SSH_ENV
     chmod 600 $SSH_ENV
-    source $SSH_ENV >/dev/null
+    source $SSH_ENV > /dev/null
     if is_mac
         ssh-add --apple-use-keychain --apple-load-keychain -q >/dev/null 2>&1 # Ensure we add keys when we start an agent
     else
@@ -28,11 +18,8 @@ end
 
 
 function fish_ssh_agent --description "Start ssh-agent if not started yet, or uses already started ssh-agent."
-    if test -z "$SSH_ENV"
-        set -xg SSH_ENV $HOME/.ssh/environment
-    end
+    set -q SSH_ENV; or set -xg SSH_ENV $HOME/.ssh/environment
+    source $SSH_ENV > /dev/null
 
-    if not cachecmd __ssh_agent_is_started
-        __ssh_agent_start
-    end
+    __ssh_agent_is_started; or __ssh_agent_start
 end
