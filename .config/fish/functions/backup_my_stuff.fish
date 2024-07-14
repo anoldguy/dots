6388d80369208b741ad59cb6a60c9fde4f97b981
Nathan Anderson <andnat@gmail.com>
2024-07-14 12:38:59 -0400

    Fix backup destination naming

--- .config/fish/functions/backup_my_stuff.fish
+++ .config/fish/functions/backup_my_stuff.fish
@@ -1,6 +1,6 @@
 function backup_my_stuff --description "Backs up my stuff to the homelab using today's date."
+    set session_date $(date +"%Y-%m-%d_%H%M")
     for dir in $(jq -r '.local[]' ~/.config/backup_dirs.json)
-      set session_date $(date +"%Y-%m-%d_%H%M")
       ssh homelab mkdir -p ~/backups/nathan-$session_date
       rsync -avz $dir homelab:~/backups/nathan-$session_date/
     end
