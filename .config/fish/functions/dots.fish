c209529fdc791e71dc5dcc1d689632cbe7f521aa
Nathan Anderson <andnat@gmail.com>
2025-04-10 17:39:19 -0400

    Update to dots; ghostty, gitconfig, etc...

--- .config/fish/functions/dots.fish
+++ .config/fish/functions/dots.fish
@@ -1,3 +1,3 @@
 function dots --wraps git --description 'Works with dotfiles'
-    /usr/bin/git --git-dir=$HOME/.dots/ --work-tree=$HOME $argv
+    /usr/bin/git --git-dir=$HOME/.dots/ --work-tree=$HOME -c status.showUntrackedFiles=no -c core.excludesFile=$HOME/.config/dots.gitignore $argv
 end
