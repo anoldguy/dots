8b2fb894960d85d3af10030e2472ee48fb90eb5b
Nathan Anderson <andnat@gmail.com>
2026-03-24 21:55:13 -0400

    --amend

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 if [ (tty) = "/dev/tty1" ]
     systemctl --user import-environment PATH
     set -x XDG_CURRENT_DESKTOP Hyprland
-    exec Hyprland
+    #exec Hyprland
+    exec start-hyprland
 end
